79588ea0e43b283760d7fdcacbeb0fff5d951969
Lucas CORDENOD <lcordeno@e2r9p8.42.fr>
2019-05-08 19:23:58 +0200

    Fixed responsive image save (filter was not scaled on montage when using responsive) - tested and OK

--- javascript/montage-processing.js
+++ javascript/montage-processing.js
@@ -1,13 +1,16 @@
 function    createMontage(width) {
     var image_elem = document.getElementById("picture-taken");
     var filter_elem = document.getElementById("filter-img-active");
+    var width_scaling = 1;
 
     image_src = image_elem.src;
     image_width = width;
+    if (document.body.clientWidth <= 545)
+        width_scaling = 1.6;
     filter_src = filter_elem.src;
-    filter_width = filter_elem.clientWidth;
-    filter_top = filter_elem.offsetTop;
-    filter_left = filter_elem.offsetLeft;
+    filter_width = filter_elem.clientWidth * width_scaling;
+    filter_top = filter_elem.offsetTop * width_scaling;
+    filter_left = filter_elem.offsetLeft * width_scaling;
     postData("../controller/create-montage-controller.php", {image_src: image_src, image_width: image_width, filter_src: filter_src, filter_width: filter_width, filter_top: filter_top, filter_left: filter_left});
 }
 
