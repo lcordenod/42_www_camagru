726811f31a0b0fe2612a73ee7b7bbaa378da6e66
Lucas CORDENOD <lcordeno@e2r9p8.42.fr>
2019-05-09 15:41:35 +0200

    Upload image checks (format and size) and error messages are done - need to pass image to processing

--- javascript/file-upload.js
+++ javascript/file-upload.js
@@ -1,17 +1,32 @@
+function    returnFileExtension(file_name)
+{
+    var file_extension = file_name.replace(/^.*\./, '');
+    return (file_extension);
+}
+
 function    handleImage(e) {
     var tgt = e.target || window.event.srcElement;
     files = tgt.files;
 
     if (FileReader && files && files.length)
     {
-        var file_reader = new FileReader();
-        file_reader.onload = function () {
-            document.getElementById('error-no-camera').style.display = "none";
-            document.getElementById('image-upload-preview').style.display = "block";
-            document.getElementById('image-upload-preview').src = file_reader.result;
-            document.getElementById('image-upload-btn').innerHTML = "Upload another image";
-            document.getElementById('upload-confirm-btn').style.display = "block";
+        var file_extension = returnFileExtension(files[0].name);
+        if ((file_extension === 'jpg' || file_extension === 'png' || file_extension === 'jpeg') && files[0].size/1024/1024 < 5)
+        {
+            console.log(files[0].size/1024/1024);
+            var file_reader = new FileReader();
+            file_reader.onload = function () {
+                document.getElementById('error-no-camera').style.display = "none";
+                document.getElementById('image-upload-preview').style.display = "block";
+                document.getElementById('image-upload-preview').src = file_reader.result;
+                document.getElementById('image-upload-btn').innerHTML = "Upload another image";
+                document.getElementById('upload-confirm-btn').style.display = "block";
+            }
+            file_reader.readAsDataURL(files[0]);
         }
-        file_reader.readAsDataURL(files[0]);
+        else if (files[0].size/1024/1024 >= 5)
+            alert("File is too big, currently " + files[0].size/1024/1024 + " mb, please upload a file lower than 5 mb");
+        else
+            alert("File format isn't correct, please upload a jpg/png/jpeg file (with size lower than 5 mb)");
     }
 }
