b9f6eeea1c4bcdea44cb04a8c21fb21111c219ca
Lucas CORDENOD <lcordeno@e3r8p11.42.fr>
2019-06-12 09:38:44 +0200

    Hidden camera tools when no camera and added delay for elements to show up at same time

--- javascript/camera.js
+++ javascript/camera.js
@@ -1,186 +1,222 @@
 var width = 480;
 var height = 0;
 var streaming = false;
-var save_button = document.getElementById('camera-save-btn');
-var retry_button = document.getElementById('camera-retry-btn');
+var save_button = document.getElementById("camera-save-btn");
+var retry_button = document.getElementById("camera-retry-btn");
 var is_image_upload = false;
 
-function    resetCamera() {
-    hidePictureTaken();
-    showCameraBox();
-    enableFilters();
-    if (document.getElementsByClassName("pictures-taken-picture").length === 0)
-    {
-        document.getElementById('message-no-pictures-taken-box').innerHTML = "Place the filter <br/> using click on camera feed<br/>👌";
-        document.getElementById('message-no-pictures-taken-box').style.backgroundColor = "rgba(78, 167, 255, 0.4)";
+function resetCamera() {
+  hidePictureTaken();
+  showCameraBox();
+  enableFilters();
+  if (document.getElementsByClassName("pictures-taken-picture").length === 0) {
+    document.getElementById("message-no-pictures-taken-box").innerHTML =
+      "Place the filter <br/> using click on camera feed<br/>👌";
+    document.getElementById(
+      "message-no-pictures-taken-box"
+    ).style.backgroundColor = "rgba(78, 167, 255, 0.4)";
+  }
+}
+
+function clearPicture() {
+  var context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  var data = canvas.toDataURL("image/png");
+  picture.setAttribute("src", data);
+}
+
+function takePicture() {
+  var context = canvas.getContext("2d");
+  if (width) {
+    if (is_image_upload === false) {
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
+      var data = canvas.toDataURL("image/png");
+    } else var data = upload_src;
+    picture.setAttribute("src", data);
+
+    if (
+      document.getElementsByClassName("pictures-taken-picture").length === 0
+    ) {
+      document.getElementById("message-no-pictures-taken-box").innerHTML =
+        "Looking good! 😎<br/>Now you can save it or retry";
+      document.getElementById(
+        "message-no-pictures-taken-box"
+      ).style.backgroundColor = "rgba(59, 255, 59, 0.4)";
     }
+    createMontage(width, picture.src);
+    hideCameraBox();
+    showPictureTaken();
+    disableFilters();
+  } else {
+    clearPicture();
+  }
 }
 
-function    clearPicture() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var data = canvas.toDataURL('image/png');
-    picture.setAttribute('src', data);
+function savePicture() {
+  saveMontage();
+  resetCamera();
 }
 
-function    takePicture() {
-    var context = canvas.getContext('2d');
-    if (width) {
-        if (is_image_upload === false)
-        {
-            canvas.width = width;
-            canvas.height = height;
-            context.drawImage(video, 0, 0, width, height);
-            var data = canvas.toDataURL('image/png');
-        }
-        else
-            var data = upload_src;
-        picture.setAttribute('src', data);
+function hideCameraBox() {
+  var camera_box = document.getElementById("camera-box");
+  var snap_button = document.getElementById("camera-snap-btn");
+  camera_box.style.display = "none";
+  snap_button.style.display = "none";
+}
 
-        if (document.getElementsByClassName("pictures-taken-picture").length === 0)
-        {
-            document.getElementById('message-no-pictures-taken-box').innerHTML = "Looking good! 😎<br/>Now you can save it or retry";
-            document.getElementById('message-no-pictures-taken-box').style.backgroundColor = "rgba(59, 255, 59, 0.4)";
-        }
-        createMontage(width, picture.src);
+function hidePicturesTakenView() {
+  var pictures_taken_view = document.getElementById("pictures-taken-view");
+  pictures_taken_view.style.display = "none";
+}
+
+function showCameraBox() {
+  var camera_box = document.getElementById("camera-box");
+  var snap_button = document.getElementById("camera-snap-btn");
+  camera_box.style.display = "block";
+  snap_button.style.display = "block";
+}
+
+function showPicturesTakenView() {
+  var pictures_taken_view = document.getElementById("pictures-taken-view");
+  pictures_taken_view.style.display = "block";
+}
+
+function hidePictureTaken() {
+  var picture_taken = document.getElementById("picture-taken");
+  var save_button = document.getElementById("camera-save-btn");
+  var retry_button = document.getElementById("camera-retry-btn");
+  picture_taken.style.display = "none";
+  save_button.style.display = "none";
+  retry_button.style.display = "none";
+}
+
+function showPictureTaken() {
+  var picture_taken = document.getElementById("picture-taken");
+  picture_taken.style.display = "block";
+  save_button.style.display = "inline-block";
+  retry_button.style.display = "inline-block";
+}
+
+function showMontageInPicturesTakenBox() {
+  var pictures_taken_box = document.getElementById("pictures-taken-box");
+  var picture_taken = document.getElementById("picture-taken");
+  var new_montage = document.createElement("img");
+  var check_existing_picture = document.getElementsByClassName(
+    "pictures-taken-picture"
+  );
+  new_montage.setAttribute("src", picture_taken.src);
+  new_montage.setAttribute("class", "pictures-taken-picture");
+
+  if (check_existing_picture === null)
+    pictures_taken_box.appendChild(new_montage);
+  else
+    pictures_taken_box.insertBefore(
+      new_montage,
+      pictures_taken_box.childNodes[0]
+    );
+}
+
+function hideNoCameraBlock() {
+  var no_camera_box = document.getElementById("no-camera-box");
+  no_camera_box.style.display = "none";
+}
+
+function showNoCameraBlock() {
+  var no_camera_box = document.getElementById("no-camera-box");
+  no_camera_box.style.display = "block";
+  hideFilters();
+}
+
+function showCameraBooth() {
+  camera_container = document.getElementsByClassName("camera-container")[0];
+  title_filters = document.getElementById("title-filters");
+  filter_selector = document.getElementById("filter-selector");
+  camera_container.style.display = "block";
+  title_filters.style.visibility = "visible";
+  filter_selector.style.visibility = "visible";
+}
+
+(function() {
+  document.getElementById("camera-snap-btn").disabled = true;
+
+  function startCamera() {
+    video = document.getElementById("camera-stream");
+    canvas = document.getElementById("canvas");
+    picture = document.getElementById("picture-taken");
+    snap_button = document.getElementById("camera-snap-btn");
+    save_button = document.getElementById("camera-save-btn");
+
+    setTimeout(showCameraBooth, 1250);
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function(stream) {
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch(function(err) {
+        console.log("No camera available: " + err);
         hideCameraBox();
-        showPictureTaken();
-        disableFilters();
-    } else {
-        clearPicture();
-    }
-}
+        hidePicturesTakenView();
+        showNoCameraBlock();
+      });
 
-function    savePicture() {
-    saveMontage();
-    resetCamera();
-}
+    video.addEventListener(
+      "canplay",
+      function(e) {
+        if (!streaming) {
+          height = video.videoHeight / (video.videoWidth / width);
+          if (isNaN(height)) {
+            height = width / (4 / 3);
+          }
+          video.setAttribute("width", width);
+          video.setAttribute("height", height);
+          canvas.setAttribute("width", width);
+          canvas.setAttribute("height", height);
+          streaming = true;
+        }
+      },
+      false
+    );
 
-function    hideCameraBox() {
-    var camera_box = document.getElementById('camera-box');
-    var snap_button = document.getElementById('camera-snap-btn');
-    camera_box.style.display = "none";
-    snap_button.style.display = "none";
-}
-
-function    hidePicturesTakenView() {
-    var pictures_taken_view = document.getElementById('pictures-taken-view');
-    pictures_taken_view.style.display = "none";
-}
-
-function    showCameraBox() {
-    var camera_box = document.getElementById('camera-box');
-    var snap_button = document.getElementById('camera-snap-btn');
-    camera_box.style.display = "block";
-    snap_button.style.display = "block";
-}
-
-function    showPicturesTakenView() {
-    var pictures_taken_view = document.getElementById('pictures-taken-view');
-    pictures_taken_view.style.display = "block";
-}
-
-function    hidePictureTaken() {
-    var picture_taken = document.getElementById('picture-taken');
-    var save_button = document.getElementById('camera-save-btn');
-    var retry_button = document.getElementById('camera-retry-btn');
-    picture_taken.style.display = "none";
-    save_button.style.display = "none";
-    retry_button.style.display = "none";
-}
-
-function    showPictureTaken() {
-    var picture_taken = document.getElementById('picture-taken');
-    picture_taken.style.display = "block";
-    save_button.style.display = "inline-block";
-    retry_button.style.display = "inline-block";
-}
-
-function    showMontageInPicturesTakenBox() {
-    var pictures_taken_box = document.getElementById('pictures-taken-box');
-    var picture_taken = document.getElementById('picture-taken');
-    var new_montage = document.createElement("img");
-    var check_existing_picture = document.getElementsByClassName("pictures-taken-picture");
-    new_montage.setAttribute("src", picture_taken.src);
-    new_montage.setAttribute("class", "pictures-taken-picture");
-
-    if (check_existing_picture === null)
-        pictures_taken_box.appendChild(new_montage);
-    else
-        pictures_taken_box.insertBefore(new_montage, pictures_taken_box.childNodes[0]);
-}
-
-function    hideNoCameraBlock() {
-    var no_camera_box = document.getElementById('no-camera-box');
-    no_camera_box.style.display = "none";
-}
-
-function    showNoCameraBlock() {
-    var no_camera_box = document.getElementById('no-camera-box');
-    no_camera_box.style.display = "block";
-    hideFilters();
-}
-
-(function () {
-    document.getElementById('camera-snap-btn').disabled = true;
-
-    function    startCamera() {
-        video = document.getElementById('camera-stream');
-        canvas = document.getElementById('canvas');
-        picture = document.getElementById('picture-taken');
-        snap_button = document.getElementById('camera-snap-btn');
-        save_button = document.getElementById('camera-save-btn');
-
-        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-            .then(function (stream) {
-                video.srcObject = stream;
-                video.play();
-            })
-            .catch(function (err) {
-                console.log("No camera available: " + err);
-                hideCameraBox();
-                hidePicturesTakenView();
-                showNoCameraBlock();
-            });
-
-        video.addEventListener('canplay', function (e) {
-            if (!streaming) {
-                height = video.videoHeight / (video.videoWidth / width);
-                if (isNaN(height)) {
-                    height = width / (4 / 3);
-                }
-                video.setAttribute('width', width);
-                video.setAttribute('height', height);
-                canvas.setAttribute('width', width);
-                canvas.setAttribute('height', height);
-                streaming = true;
-            }
-        }, false);
-
-        snap_button.addEventListener('click', function (e) {
-            picture.style.width = width;
-            var filter_elem = document.getElementById("filter-img-active");
-            takePicture();
-            e.preventDefault();
-        }, false);
-
-        hidePictureTaken();
-        clearPicture();
-    }
-
-    retry_button.addEventListener('click', function (e) {
-        resetCamera();
+    snap_button.addEventListener(
+      "click",
+      function(e) {
+        picture.style.width = width;
+        var filter_elem = document.getElementById("filter-img-active");
+        takePicture();
         e.preventDefault();
-    }, false);
+      },
+      false
+    );
 
-    save_button.addEventListener('click', function (e) {
-        savePicture();
-        showMontageInPicturesTakenBox();
-        document.getElementById('message-no-pictures-taken-box').style.display = "none";
-        e.preventDefault();
-    }, false);
+    hidePictureTaken();
+    clearPicture();
+  }
 
-    window.addEventListener('load', startCamera, false);
+  retry_button.addEventListener(
+    "click",
+    function(e) {
+      resetCamera();
+      e.preventDefault();
+    },
+    false
+  );
+
+  save_button.addEventListener(
+    "click",
+    function(e) {
+      savePicture();
+      showMontageInPicturesTakenBox();
+      document.getElementById("message-no-pictures-taken-box").style.display =
+        "none";
+      e.preventDefault();
+    },
+    false
+  );
+
+  window.addEventListener("load", startCamera, false);
 })();
--- javascript/filters.js
+++ javascript/filters.js
@@ -5,40 +5,37 @@ var filter_active = document.getElementById("filter-img-active");
 var filters_enabled = true;
 var filter_current = false;
 
-function    unselectOtherFilters(array, exclude) {
-    for (var i = 0; i < array.length; i++)
-    {
-        if (array[i] !== exclude)
-            array[i].style.opacity = "0.5";
-    }
+function unselectOtherFilters(array, exclude) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] !== exclude) array[i].style.opacity = "0.5";
+  }
 }
 
-(function () {
-    for (var i = 0; i < list_filters.length; i++)
-    {
-        list_filters[i].addEventListener('click', function (e) {
-            if (filters_enabled === true)
-            {
-                unselectOtherFilters(list_filters, this);
-                this.style.opacity = "1";
-                filter_selected = true;
-                filter_current = this;
-                if (this !== list_filters[0])
-                {
-                    filter_active.style.display = "block";
-                    filter_active.src = this.src;
-                }
-                else
-                    filter_active.style.display = "none";
-                if (filter_selected == true)
-                {
-                    document.getElementById('camera-snap-btn').disabled = false;
-                    document.getElementById('message-no-pictures-taken-box').innerHTML = "Place the filter <br/> using click on camera feed<br/>👌";
-                }
-                e.preventDefault();
-            }
-        }, false);
-    }
+(function() {
+  for (var i = 0; i < list_filters.length; i++) {
+    list_filters[i].addEventListener(
+      "click",
+      function(e) {
+        if (filters_enabled === true) {
+          unselectOtherFilters(list_filters, this);
+          this.style.opacity = "1";
+          filter_selected = true;
+          filter_current = this;
+          if (this !== list_filters[0]) {
+            filter_active.style.display = "block";
+            filter_active.src = this.src;
+          } else filter_active.style.display = "none";
+          if (filter_selected == true) {
+            document.getElementById("camera-snap-btn").disabled = false;
+            document.getElementById("message-no-pictures-taken-box").innerHTML =
+              "Place the filter <br/> using click on camera feed<br/>👌";
+          }
+          e.preventDefault();
+        }
+      },
+      false
+    );
+  }
 })();
 
 // Moving the filter image on top of the camera-box
@@ -48,57 +45,56 @@ var box = document.querySelector("#camera-box");
 box.addEventListener("click", getClickPosition, false);
 
 function getClickPosition(e) {
-    var filter_display = filter_active.style.display;
-    var parentPosition = getPosition(e.currentTarget);
-    var xPosition = e.clientX - parentPosition.x - (filter.clientWidth / 2);
-    var yPosition = e.clientY - parentPosition.y - (filter.clientHeight / 2);
+  var filter_display = filter_active.style.display;
+  var parentPosition = getPosition(e.currentTarget);
+  var xPosition = e.clientX - parentPosition.x - filter.clientWidth / 2;
+  var yPosition = e.clientY - parentPosition.y - filter.clientHeight / 2;
 
-    if (filter_display == "block")
-    {
-        filter.style.left = xPosition + "px";
-        filter.style.top = yPosition + "px";
-    }
+  if (filter_display == "block") {
+    filter.style.left = xPosition + "px";
+    filter.style.top = yPosition + "px";
+  }
 }
 
 function getPosition(el) {
-    var xPos = 0;
-    var yPos = 0;
+  var xPos = 0;
+  var yPos = 0;
 
-    while (el) {
+  while (el) {
     if (el.tagName == "BODY") {
-        var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
-        var yScroll = el.scrollTop || document.documentElement.scrollTop;
+      var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+      var yScroll = el.scrollTop || document.documentElement.scrollTop;
 
-        xPos += (el.offsetLeft - xScroll + el.clientLeft);
-        yPos += (el.offsetTop - yScroll + el.clientTop);
-        } else {
-            xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
-            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
-        }
-        el = el.offsetParent;
+      xPos += el.offsetLeft - xScroll + el.clientLeft;
+      yPos += el.offsetTop - yScroll + el.clientTop;
+    } else {
+      xPos += el.offsetLeft - el.scrollLeft + el.clientLeft;
+      yPos += el.offsetTop - el.scrollTop + el.clientTop;
     }
-    return {
-        x: xPos,
-        y: yPos
-    };
+    el = el.offsetParent;
+  }
+  return {
+    x: xPos,
+    y: yPos
+  };
 }
 
-function    disableFilters() {
-    unselectOtherFilters(list_filters, false);
-    filters_enabled = false;
+function disableFilters() {
+  unselectOtherFilters(list_filters, false);
+  filters_enabled = false;
 }
 
-function    enableFilters() {
-    filters_enabled = true;
-    filter_current.style.opacity = "1";
+function enableFilters() {
+  filters_enabled = true;
+  filter_current.style.opacity = "1";
 }
 
-function    hideFilters() {
-    document.getElementById('title-filters').style.display = "none";
-    document.getElementById('filter-selector').style.display = "none";
+function hideFilters() {
+  document.getElementById("title-filters").style.display = "none";
+  document.getElementById("filter-selector").style.display = "none";
 }
 
-function    showFilters() {
-    document.getElementById('title-filters').style.display = "block";
-    document.getElementById('filter-selector').style.display = "block";
+function showFilters() {
+  document.getElementById("title-filters").style.display = "block";
+  document.getElementById("filter-selector").style.display = "block";
 }
